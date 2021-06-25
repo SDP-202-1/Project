6ba335fac3cf436915311465e2c273c4cff346f8
ЛаптевС <@N215-4>
2021-06-25 13:28:07 +0600

    Choice

--- Source.cpp
+++ Source.cpp
@@ -10,8 +10,11 @@ int main()
 {
 	SetConsoleCP(1251); SetConsoleOutputCP(1251);
 	srand(time(NULL));
+	string choice;
 	cout << "Teacher work" << endl;
 	cout << "Savva" << endl;
 	cout << "Aboba" << endl;
 	cout << "Abiba" << endl;
+	cout << "Make a Choice" << endl;
+	cin >> choice; cout << "Åhe choice is made" << endl;
 }
