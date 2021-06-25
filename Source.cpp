565a234c97b3a0b05cb910a3e62ab158609d7423
ITstart <ITstart@N215-15>
2021-06-25 13:34:05 +0600

    add v

--- Source.cpp
+++ Source.cpp
@@ -10,7 +10,15 @@ int main()
 {
 	SetConsoleCP(1251); SetConsoleOutputCP(1251);
 	srand(time(NULL));
+	string choice;
 	cout << "Teacher work" << endl;
+
 	cout << "Viktor" << endl;
 	cout << endl;
+
+	cout << "Savva" << endl;
+	cout << "Aboba" << endl;
+	cout << "Abiba" << endl;
+	cout << "Make a Choice" << endl;
+	cin >> choice; cout << "The choice is made" << endl;
 }
