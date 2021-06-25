a524e00a53c457ce2960364696f5385345149086
НикитинН <@N209-10>
2021-06-25 13:44:03 +0600

    7777

--- Source.cpp
+++ Source.cpp
@@ -26,7 +26,7 @@ int main()
 	cin >> choice; cout << "The choice is made" << endl;
 	cout << "Viktor" << endl;
 	cout << "Daniil" << endl;
-	Sleep(1000);
+	Sleep(10000);
 	while (true) {
 		cout << choice;
 	}
