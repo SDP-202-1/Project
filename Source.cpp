964111c0cc228d13d8198d645cbba53e402e977b
СибгатулинР <@N206-9>
2021-06-25 13:30:59 +0600

    nothing

--- Source.cpp
+++ Source.cpp
@@ -11,6 +11,5 @@ int main()
 	SetConsoleCP(1251); SetConsoleOutputCP(1251);
 	srand(time(NULL));
 	cout << "Teacher work" << endl;
-	cout << "Aboba" << endl;
-	cout << "Abiba" << endl;
+	cout << "Viktor" << endl;
 }
